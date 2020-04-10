3b4d3b935e35dece8b6e164d0b9f53e130d9e8fd
Robert Nicholas Dill Jr <nickjr@dillfamily.org>
2020-04-10 13:25:19 -0400

    Added countries as a searchable result

--- commands/coronavirus.js
+++ commands/coronavirus.js
@@ -117,6 +117,7 @@ module.exports = {
 				state = state + args[i] + " ";
 			}
 			state = state.trim();
+			var country = state;
 			//message.channel.send(`Your word was: \`${state}\``);
 			var stateArray = [];
 			for (var i = 0; i < usStateList.length; i++) {
@@ -127,49 +128,129 @@ module.exports = {
 				}
 			}
 			//message.channel.send(`${stateArray.length} number of results`);
+			var stateResults = true;
 			if (stateArray.length <= 0) {
+				// No states results, check for country now
+				//return message.channel.send(`No results! Please check your spelling.`);
+				//message.channel.send(`No state results, searching for matching countries...`);
+				stateResults = false;
+			}
+			else {
+				var winnerUnix = 0;
+				var winner;
+
+				for (var i = 0; i < stateArray.length; i++) {
+					var timeCheck = new Date(`${stateArray[i].Date}`).valueOf();
+					if (timeCheck >= winnerUnix) {
+						winnerUnix = timeCheck;
+						winner = stateArray[i].Date;
+					}
+	        //message.channel.send(`${stateArray[i].Date}`);
+				}
+
+				for (var i = 0; i < stateArray.length; i++) {
+					if (stateArray[i].Date == winner) {
+						const stateEmbed = {
+								title: "Provided By Johns Hopkins University",
+								description: "via the [covid19api](https://covid19api.com/)",
+								url: "https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6",
+								color: 5159289,
+								timestamp: new Date(`${stateArray[i].Date}`),
+								fields: [
+										{
+											name: 'Confirmed',
+											value: `${stateArray[i].Confirmed}`,
+											inline: true
+										},
+										{
+											name: 'Active',
+											value: `${stateArray[i].Active}`,
+											inline: true
+										},
+										{
+											name: 'Deaths',
+											value: `${stateArray[i].Deaths}`,
+											inline: true
+										},
+										{
+											name: 'Recovered',
+											value: `${stateArray[i].Recovered}`,
+											inline: true
+										}
+									],
+								footer: {
+									"text": "Updated",
+									"icon_url": "https://upload.wikimedia.org/wikipedia/commons/0/04/Corona_Extra_beer_bottle_%282019%29.png"
+								},
+								author: {
+									"name": `${stateArray[i].Province} Coronavirus Stats`,
+									"icon_url": "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/48/flag-for-united-states_1f1fa-1f1f8.png"
+								}
+						};
+						message.channel.send({ embed: stateEmbed });
+					}
+				}
+			}
+
+
+
+			var countryList = await fetch('https://api.covid19api.com/countries').then(response => response.json());
+			var countrySlug = "";
+			for (var i = 0; i < countryList.length; i++) {
+				if (countryList[i] && countryList[i].Country) {
+					if (countryList[i].Country.toLowerCase() == country.toLowerCase() || countryList[i].Slug.toLowerCase() == country.toLowerCase()) {
+						countrySlug = countryList[i].Slug;
+						break;
+					}
+				}
+			}
+
+			if (countrySlug == "" && !stateResults) {
 				return message.channel.send(`No results! Please check your spelling.`);
 			}
-
-			var winnerUnix = 0;
-			var winner;
-
-			for (var i = 0; i < stateArray.length; i++) {
-				var timeCheck = new Date(`${stateArray[i].Date}`).valueOf();
-				if (timeCheck >= winnerUnix) {
-					winnerUnix = timeCheck;
-					winner = stateArray[i].Date;
-				}
-        //message.channel.send(`${stateArray[i].Date}`);
+			else if (countrySlug == "") {
+				return;
 			}
 
-			for (var i = 0; i < stateArray.length; i++) {
-				if (stateArray[i].Date == winner) {
-					const stateEmbed = {
+			var countryStats = await fetch(`https://api.covid19api.com/live/country/${countrySlug}/status/confirmed`).then(response => response.json());
+			var countryCurrentUnix = 0;
+			var countryCurrent;
+
+			for (var i = 0; i < countryStats.length; i++) {
+				var timeCheck = new Date(`${countryStats[i].Date}`).valueOf();
+				if (timeCheck >= countryCurrentUnix) {
+					countryCurrentUnix = timeCheck;
+					countryCurrent = countryStats[i].Date;
+				}
+			}
+
+			for (var i = 0; i < countryStats.length; i++) {
+				if (countryStats[i].Date == countryCurrent) {
+					const countryEmbed = {
 							title: "Provided By Johns Hopkins University",
 							description: "via the [covid19api](https://covid19api.com/)",
 							url: "https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6",
 							color: 5159289,
-							timestamp: new Date(`${stateArray[i].Date}`),
+							timestamp: new Date(`${countryStats[i].Date}`),
 							fields: [
 									{
 										name: 'Confirmed',
-										value: `${stateArray[i].Confirmed}`,
+										value: `${countryStats[i].Confirmed}`,
 										inline: true
 									},
 									{
 										name: 'Active',
-										value: `${stateArray[i].Active}`,
+										value: `${countryStats[i].Active}`,
 										inline: true
 									},
 									{
 										name: 'Deaths',
-										value: `${stateArray[i].Deaths}`,
+										value: `${countryStats[i].Deaths}`,
 										inline: true
 									},
 									{
 										name: 'Recovered',
-										value: `${stateArray[i].Recovered}`,
+										value: `${countryStats[i].Recovered}`,
 										inline: true
 									}
 								],
@@ -178,11 +259,11 @@ module.exports = {
 								"icon_url": "https://upload.wikimedia.org/wikipedia/commons/0/04/Corona_Extra_beer_bottle_%282019%29.png"
 							},
 							author: {
-								"name": `${stateArray[i].Province} Coronavirus Stats`,
-								"icon_url": "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/48/flag-for-united-states_1f1fa-1f1f8.png"
+								"name": `${countryStats[i].Country} Coronavirus Stats`,
+								"icon_url": `https://www.countryflags.io/${countryStats[i].CountryCode}/shiny/64.png`
 							}
 					};
-					message.channel.send({ embed: stateEmbed });
+					return message.channel.send({ embed: countryEmbed });
 				}
 			}
 		}
